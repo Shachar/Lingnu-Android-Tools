2643ff9bdf69638f1144c86bbfbf3482209d1797
Asaf Ohaion <asaf@zion.localdomain>
2011-04-11 18:42:01 +0300

    new_strings : basicly works

--- new_strings.cpp
+++ new_strings.cpp
@@ -9,6 +9,7 @@
 
 #define BUFSIZE 4096
 
+enum diff_operation {NOP, ADDED, DELETED, MODIFIED};
 
 using namespace std;
 
@@ -89,6 +90,24 @@ void parse_strings_xml(char* filename, map<string, string>& string_map)
     }	    
 }
 
+void diff(map<string,string>& base, map<string,string>& other, map<string,diff_operation>& results)
+{
+    map<string, string>::iterator p;
+
+    // search new strings:
+    for(p = other.begin(); p != other.end(); p++)
+	if ( base.find(p->first) == base.end() )
+	    results[p->first] = ADDED;
+    
+    // search modified / deleted strings:
+    for(p = base.begin(); p != base.end(); p++) {
+	if ( other.find(p->first) == other.end() )
+	    results[p->first] = DELETED;
+	else if ( p->second != other[p->first] )
+	    results[p->first] = MODIFIED;
+    }
+
+}
 
 int main(int argc, char * argv[])
 {
@@ -97,24 +116,52 @@ int main(int argc, char * argv[])
 	return 0;
     }
 
+    // parse
+
     cout<<"parsing "<<argv[1]<<endl;
     map<string,string> old_strings_en;
     parse_strings_xml(argv[1], old_strings_en);
 
     cout<<"parsing "<<argv[2]<<endl;
     map<string,string> new_strings_en;
-    parse_strings_xml(argv[2], old_strings_en);
+    parse_strings_xml(argv[2], new_strings_en);
 
     cout<<"parsing "<<argv[3]<<endl;
     map<string,string> old_strings_iw;
-    parse_strings_xml(argv[3], old_strings_en);
+    parse_strings_xml(argv[3], old_strings_iw);
 
-/*    map<string, string>::iterator p;
+    // diff
+
+    map<string,diff_operation> diff_old_new;
+    diff(old_strings_en, new_strings_en, diff_old_new);
+
+    // output
     
-    cout<<"Printing Strings.."<<endl;
-    for(p = old_strings_en.begin(); p != old_strings_en.end(); p++) {
-	cout << p->first << " = " << p->second << endl;
-	}	    */
+    cout<<"<?xml version=\"1.0\" encoding=\"utf-8\"?>"<<endl;
+    cout<<"<resources>"<<endl;
 
+    map<string, string>::iterator p;
+    for(p = old_strings_iw.begin(); p != old_strings_iw.end(); p++) {
+	diff_operation diff_op = NOP;
+	if ( diff_old_new.find(p->first) != diff_old_new.end() )
+	    diff_op = diff_old_new[p->first];
+	
+	if (diff_op == DELETED)
+	    continue;
+
+	cout << "<string name=\""<<p->first<<"\">";
+
+	if (diff_op == MODIFIED)
+	    cout<<"MODIFIED : ";	
+	
+	cout<<p->second<<"</string>"<<endl;
+    }
+
+    map<string, diff_operation>::iterator p_diff;
+    for(p_diff = diff_old_new.begin(); p_diff != diff_old_new.end(); p_diff++)
+	if (p_diff->second == ADDED)
+	    cout << "<string name=\""<<p_diff->first<<"\">"<<"NEW"<<"</string>"<<endl;
+    
+    cout<<"</resources>"<<endl;
     return 0;
 }
